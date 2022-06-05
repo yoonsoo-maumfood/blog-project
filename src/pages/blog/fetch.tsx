34accea4924e76b7cdde5442076b19bf60df4a5b
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-06 01:50:33 +0900

    FEAT/fetch에 comments도 같이 다뤄지게 수정
    comment를 post와 무관하게 접근하는 경우는 없을 것이므로, interface Post에 comments를 추가해서 한꺼번에 다뤄지도록 만듦
    그리고 데이터가 제대로 담기기 위해서는 post가 완전히 정리된 이후에 comment를 그에 맞게 맞춰줄 필요가 있음. 따라서 async 함수를 활용하기로 함.

--- src/pages/blog/fetch.tsx
+++ src/pages/blog/fetch.tsx
@@ -1,39 +1,72 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 interface Post {
-  userId: number,
-  id: number,
-  title: string,
-  body: string,
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  comments: Comment[];
 }
 
-const Fetch = () => {
-  const [ posts, setPosts ] = useState<Post[]>([]);
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
 
-  useEffect( () => {
-    const fetchPromise = fetch('https://jsonplaceholder.typicode.com/posts');
-    fetchPromise.then( response => {
-      return response.json();
-    }).then(
-      json => {
-        const newPosts = Array<Post>();
-        for ( const post of json) {
-          newPosts.push(post);
-        }
-        setPosts(newPosts);
-      }
-    )
-  }, [])
+const getPosts = async (
+  postsUrl: string,
+  commentsUrl: string,
+  setPosts: Function
+) => {
+  const newPosts = Array<Post>();
+
+  const fetchPosts = await fetch(postsUrl);
+  for (const post of await fetchPosts.json()) {
+    post.comments = []; //post.comment는 json에서 초기화되지 않으므로
+    newPosts.push(post);
+  }
+
+  const fetchComments = await fetch(commentsUrl);
+  for (const comment of await fetchComments.json()) {
+    newPosts[comment.postId - 1].comments.push(comment);
+  }
+  setPosts(newPosts);
+};
+
+const Fetch = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    getPosts(
+      "https://jsonplaceholder.typicode.com/posts",
+      "https://jsonplaceholder.typicode.com/comments",
+      setPosts
+    );
+  }, []);
 
   return (
     <div>
       <h1>Fetch</h1>
-      { posts.length ? posts.map( (post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </div>
-      )) : <h2>로딩중</h2>}
+      {posts.length ? (
+        posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <div>
+              <ul>
+                {post.comments.map((comment) => (
+                  <li key={comment.id}>{comment.body}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h2>로딩중</h2>
+      )}
     </div>
   );
 };
