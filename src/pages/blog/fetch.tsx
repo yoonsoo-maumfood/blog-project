4ac4e87a73c5aa555f323d8ad5b959c9ac9bf6c1
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-06 01:12:57 +0900

    FEAT/fetch 이용해서 blog 긁어오기
    fetch.tsx문서 수정함
    일단은 그냥 useState를 이용해서 구현함. 앞으로 해야 할 일들
    
    1. 댓글 정보도 fetch해와서 연결해주기
    2. custom hook을 이용해서 더 유연하게 적용할 수 있도록 하기
    3. fetch실패했을때 문구 넣기

--- src/pages/blog/fetch.tsx
+++ src/pages/blog/fetch.tsx
@@ -1,8 +1,41 @@
+import { useState, useEffect } from 'react';
+
+interface Post {
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
+}
 
 const Fetch = () => {
+  const [ posts, setPosts ] = useState<Post[]>([]);
+
+  useEffect( () => {
+    const fetchPromise = fetch('https://jsonplaceholder.typicode.com/posts');
+    fetchPromise.then( response => {
+      return response.json();
+    }).then(
+      json => {
+        const newPosts = Array<Post>();
+        for ( const post of json) {
+          newPosts.push(post);
+        }
+        setPosts(newPosts);
+      }
+    )
+  }, [])
+
   return (
-    <h1>Fetch</h1>
-  )
-}
+    <div>
+      <h1>Fetch</h1>
+      { posts.length ? posts.map( (post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      )) : <h2>로딩중</h2>}
+    </div>
+  );
+};
 
 export default Fetch;
