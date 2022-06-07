b1cf80693f0cab2b0a7c0260b5dd3920da877eec
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-07 10:56:39 +0900

    FEAT/axios 페이지 1차 완성
    axios를 설치했다.
    axios.get을 용해서 간단하게 posts를 받아오고 출력하는 기능을 구현하였음.
    마찬가지로 예외처리와 댓글처리를 함께 넣을 예정

--- src/pages/blog/axios.tsx
+++ src/pages/blog/axios.tsx
@@ -1,8 +1,35 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 const Axios = () => {
+  const [posts, setPosts] = useState(Array<Post>());
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setPosts( response.data );
+      });
+  }, []);
+
   return (
-    <h1>Axios</h1>
-  )
-}
+    <div>
+      <h1>Axios</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Axios;
