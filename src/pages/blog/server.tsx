d1643da5ac5b3c773afc9d6b751d11b02b7eb403
HaYoonsoo <yoonsoo@maumfood.co.kr>
2022-06-07 11:22:07 +0900

    FEAT/Server.tsx Static generation 완성
    getStaticProps 부분은 axios를 이용하여 구현했다. 마찬가지로 댓글과 예외처리등은 하지 않음.
    fetch 기반으로 코드를 다시 다듬은 다음에, 전체에 옮길 예정

--- src/pages/blog/server.tsx
+++ src/pages/blog/server.tsx
@@ -1,8 +1,37 @@
+import axios from "axios";
 
-const Server = () => {
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface PropsWPosts {
+  posts: Post[];
+}
+
+const Server = ( { posts }: PropsWPosts  ) => {
   return (
+    <div>
     <h1>Server</h1>
+    {posts.map((post) => (
+      <div key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </div>
+    ))}
+
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts: (await axios.get("https://jsonplaceholder.typicode.com/posts")).data,
+    }
+  }
 }
 
 export default Server;
